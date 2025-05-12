20f60c21515517a1d9a7e7f364dd033f87a3bafa
Demonslayerrrr <u132_ilinai_waw@technischools.com>
2025-05-12 18:31:57 +0200

    final update

--- backend/app.py
+++ backend/app.py
@@ -1,37 +1,47 @@
 from flask import Flask, jsonify, request
 from http import HTTPStatus
+from flask.views import MethodView
 from controller import WeatherController
 from repository import WeatherRepository
 from pydantic import ValidationError
 
 app = Flask(__name__)
+
 repository = WeatherRepository()
 controller = WeatherController(repository)
 
-@app.post('/create_data')
-def create_data():
-    try:
-        data = request.get_json()
-        print(data)
-        controller.create_data(data)
-        return jsonify({"message": "Data created"}), HTTPStatus.CREATED
-    except ValidationError as e:
-        return jsonify({"message": str(e)}), HTTPStatus.UNPROCESSABLE_ENTITY
+class CreateDataView(MethodView):
+    def __init__(self, controller: WeatherController):
+        self.controller = controller
 
-@app.get('/get_data')
-def get_data():
-    try:
-        date = request.args.get('date')
-        time = request.args.get('time')
-        
-        if not date or not time:
-            return jsonify({"message": "Missing 'date' or 'time' parameter"}), HTTPStatus.BAD_REQUEST
+    def post(self):
+        try:
+            data = request.get_json()
+            print(data)
+            self.controller.create_data(data)
+            return jsonify({"message": "Data created"}), HTTPStatus.CREATED
+        except ValidationError as e:
+            return jsonify({"message": str(e)}), HTTPStatus.UNPROCESSABLE_ENTITY
 
-        data = controller.get_data(date, time)
-        return jsonify(data.model_dump()), HTTPStatus.OK    
-    except Exception as e:
-        return jsonify({"message": str(e)}), HTTPStatus.NOT_FOUND
+class GetDataView(MethodView):
+    def __init__(self, controller: WeatherController):
+        self.controller = controller
 
+    def get(self):
+        try:
+            date = request.args.get('date')
+            time = request.args.get('time')
+            
+            if not date or not time:
+                return jsonify({"message": "Missing 'date' or 'time' parameter"}), HTTPStatus.BAD_REQUEST
+
+            data = self.controller.get_data(date, time)
+            return jsonify(data.model_dump()), HTTPStatus.OK    
+        except Exception as e:
+            return jsonify({"message": str(e)}), HTTPStatus.NOT_FOUND
+
+app.add_url_rule('/create_data', view_func=CreateDataView.as_view('create_data', controller=controller))
+app.add_url_rule('/get_data', view_func=GetDataView.as_view('get_data', controller=controller))
 
 if __name__ == '__main__':
     app.run(debug=True)
